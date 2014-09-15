b56bf6933c92fc2ad6c4e220df0e478fe870ce9b
Emily Kelton <emilyk000@gmail.com>
2014-09-15 14:46:58 -0700

    changed bio

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
    
+	// function to show portfolio overlays on hover
+
     $(".item").hover(function(){
-        $(this).find("div.slideInfo").slideDown("slow");
+        $(this).find(".slideInfo").slideDown("slow");
     },function(){
-        $(this).find("div.slideInfo").slideUp("slow");    
-    });
- 
+        $(this).find(".slideInfo").slideUp("slow");    
+    }); //overlay function end
+
+ 	//  function to show social media icons on click
+ 		$(".followClick").hover(function(){
+ 				$(".follow").show("slide", { direction: "right" }, 1000);
+ 			},function(){
+ 				$(".follow").hide("slide", { direction: "left" }, 1000);
+ 			}); // end social media function
     
-});
+}); // document ready end
