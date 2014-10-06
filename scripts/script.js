4e065427940b3aa472c23aaa806d025e714c04e7
Emily Kelton <emilyk000@gmail.com>
2014-10-06 14:22:31 -0700

    delayed portfolio transitions

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,31 @@ $(document).ready(function(){
    
 	// function to show portfolio overlays on hover
 
-    $(".item").hover(function(){
+var timer;
+var delay = 1000;
+
+$(".item").hover(function(){
+    // on mouse in, start a timeout
+
+    timer = setTimeout(function() {
+        // do your stuff here
+        $(this).find(".overlayInfo").slideDown("slow");
+    },function(){
+        $(this).find(".overlayInfo").slideUp("slow");    
+    }, delay);
+}, function() {
+    // on mouse out, cancel the timer
+    clearTimeout(timer);
+});
+
+//my overlay function
+   $(".item").hover(function(){
         $(this).find(".overlayInfo").slideDown("slow");
     },function(){
         $(this).find(".overlayInfo").slideUp("slow");    
     }); //overlay function end
 
+   //function for scrolling transitions
  	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
